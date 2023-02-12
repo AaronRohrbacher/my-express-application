cd023c447df8fcd1dc652f91803627e8f13122e3
Aaron Rohrbacher <arjazz@icloud.com>
2023-02-12 15:52:44 -0800

    add local dev environment

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ const AWS = require('aws-sdk');
 
 
 const USERS_TABLE = process.env.USERS_TABLE;
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+	
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDb;
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  })
+  console.log(dynamoDb);
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+};
 
 app.use(bodyParser.json({ strict: false }));
 
@@ -43,9 +54,9 @@ app.get('/users/:userId', function (req, res) {
 app.post('/users', function (req, res) {
   const { userId, name } = req.body;
   if (typeof userId !== 'string') {
-    res.status(400).json({ error: '"userId" must be a string' });
+    return res.status(400).json({ error: '"userId" must be a string' });
   } else if (typeof name !== 'string') {
-    res.status(400).json({ error: '"name" must be a string' });
+   return res.status(400).json({ error: '"name" must be a string' });
   }
 
   const params = {
@@ -59,7 +70,7 @@ app.post('/users', function (req, res) {
   dynamoDb.put(params, (error) => {
     if (error) {
       console.log(error);
-      res.status(400).json({ error: 'Could not create user' });
+      return res.status(400).json({ error: 'Could not create user' });
     }
     res.json({ userId, name });
   });
